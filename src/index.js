6fffb435bb12edf88109534e6be96a1aa64737dd
mazhengrong25 <mazhengrong25@163.com>
2021-07-21 11:44:04 +0800

    过期票管理

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
+/*
+ * @Description: 
+ * @Author: mzr
+ * @Date: 2021-07-08 10:22:52
+ * @LastEditTime: 2021-07-19 10:05:25
+ * @LastEditors: mzr
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.scss';
+
+// 引入页面 
+import Index from "./page/index"; // 加载页
+import searchList from './page/searchList/searchList'; // 首页
+
+
+import { ConfigProvider } from "antd"; // 全局化配置
+import zhCN from "antd/es/locale/zh_CN";
+
+import axios from "./api/api";
+import moment from 'moment';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+React.Component.prototype.$axios = axios
+React.Component.prototype.$moment = moment
+
+React.$filterUrlParams = function () {
+  let str = window.location.search.replace("?", "");
+  let arr = str.split("&");
+  let obj = {};
+  arr.forEach((e) => {
+    let key = e.split("=");
+    obj[key[0]] = key[1];
+  });
+  return obj;
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ConfigProvider locale={zhCN}>
+    <Router>
+      {/* 加载页 */}
+      <Route exact path="/" component={Index}></Route>
+      {/* 分页查询 */}
+      <Route path="/expiredList" component={searchList}></Route>
+    </Router>
+  </ConfigProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
